5292c3da63118431903c453dae0e1347d8413cd7
Michael Arnaldi <arnaldimichael@gmail.com>
2022-06-05 14:16:18 +0000

    add gitpod banner

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+# Tackling the hardest problems in JavaScript [![Gitpod Ready-to-Code](https://img.shields.io/badge/Gitpod-ready--to--code-908a85?logo=gitpod)](https://gitpod.io/#https://github.com/mikearnaldi/workers-conf-2022)
+
 Slides deployed at: [https://mikearnaldi.github.io/workers-conf-2022/](https://mikearnaldi.github.io/workers-conf-2022/)
---
+++ examples/effect/07-http.ts
@@ -0,0 +1,52 @@
+import * as Effect from "@effect/core/io/Effect";
+import * as Layer from "@effect/core/io/Layer";
+import * as Schedule from "@effect/core/io/Schedule";
+import * as Duration from "@tsplus/stdlib/data/Duration";
+import * as Either from "@tsplus/stdlib/data/Either";
+import { pipe } from "@tsplus/stdlib/data/Function";
+import { Service } from "@tsplus/stdlib/service/Service";
+
+export class FetchError {
+  readonly _tag = "FetchError";
+  constructor(readonly error: unknown) {}
+}
+
+export class JsonBodyError {
+  readonly _tag = "JsonBodyError";
+  constructor(readonly error: unknown) {}
+}
+
+export const makeHttpService = () => {
+  const request = (input: RequestInfo | URL, init?: RequestInit | undefined) =>
+    Effect.asyncInterrupt<never, FetchError, Response>((resume) => {
+      const controller = new AbortController();
+      fetch(input, { ...(init ?? {}), signal: controller.signal }).then((response) => {
+        resume(Effect.succeed(() => response));
+      }).catch((error) => {
+        resume(Effect.fail(() => new FetchError(error)));
+      });
+      return Either.left(Effect.succeed(() => {
+        controller.abort();
+      }));
+    });
+  const jsonBody = (input: Response) =>
+    Effect.tryCatchPromise(
+      (): Promise<unknown> => input.json(),
+      (error) => new JsonBodyError(error)
+    );
+  const defaultRetrySchedule = pipe(
+    Schedule.exponential(Duration.millis(10), 2.0),
+    Schedule.either(Schedule.spaced(() => Duration.seconds(1))),
+    Schedule.compose(Schedule.elapsed),
+    Schedule.whileOutput(Duration.lowerThenOrEqual(Duration.seconds(30)))
+  );
+  return {
+    request,
+    jsonBody,
+    defaultRetrySchedule
+  };
+};
+
+export interface HttpService extends ReturnType<typeof makeHttpService> {}
+export const HttpService = Service.Tag<HttpService>();
+export const LiveHttpService = Layer.fromValue(HttpService, makeHttpService);
